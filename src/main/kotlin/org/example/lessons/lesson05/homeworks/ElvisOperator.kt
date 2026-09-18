6b2181253e09337615bbf9def0027114a81733d3
Doroshko Oleg <doroshko.oleg@rwb.ru>
2026-09-18 17:06:37 +0300

    Initial commit

--- src/main/kotlin/org/example/lessons/lesson05/homeworks/ElvisOperator.kt
+++ src/main/kotlin/org/example/lessons/lesson05/homeworks/ElvisOperator.kt
@@ -1,5 +1,24 @@
 package org.example.lessons.lesson05.homeworks
 
 fun main() {
+    // Задача 1
+    val initialIntensity: Double = 100.0
+    val dampingCoefficient: Double? = null
+    val actualCoefficient: Double = dampingCoefficient ?: 0.5
+    val finalIntensity: Double = initialIntensity * actualCoefficient
+    println(finalIntensity)
 
+    //Задача 2
+    val deliveryCost: Double = 20.0
+    val cargoCost: Double? = null
+    val actualCargoCost: Double = cargoCost ?: 50.0
+    val insuranceCost: Double = actualCargoCost * 0.005
+    val totalCost: Double = deliveryCost + insuranceCost
+    println(totalCost)
+
+    //Задача 3
+    val atmosphericPressure: String? = null
+    val errorMessage = "Error, atmospheric pressure is lost"
+    val result = atmosphericPressure ?: errorMessage
+    println(result)
 }
